87d08e17c28b951d40895fcae72b5d31bf65ef8a
AtharvaKalekar <atharvakalekar06@gmail.com>
2024-12-31 13:08:24 +0530

    Add files via upload

---
+++ whilee.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=1;
+    do
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    while(i<=10);
+
+    for(int i=1;i<=10;i++)
+    {
+        cout<<i<<endl;
+    }
+}
