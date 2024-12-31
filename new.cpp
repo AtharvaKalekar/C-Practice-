dc8eca3c34704490dc7cd44c054ffa3171d078ad
AtharvaKalekar <atharvakalekar06@gmail.com>
2024-12-31 13:06:22 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include <iomanip> 
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"enter any 2 numbers";
+    cin>>a>>b;
+
+    if (a>b)
+    {
+        cout<<"A is greater";
+    }
+    else
+    {
+        cout<<"B is greater";
+    }
+
+
+}
