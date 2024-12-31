5f23cfd0c6c2544b06d4985856e9afc3eaabe861
AtharvaKalekar <atharvakalekar06@gmail.com>
2024-12-31 13:08:04 +0530

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    int i=1;   //counter init step 1
+
+    while(i<=10)  //condition step 2
+    {
+        cout<<i<<endl;  // Step 3
+        i++;  //Step 4
+    }
+}
+
