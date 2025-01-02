b0c2ca1ef680779167e22e663e5aa4843c29c124
AtharvaKalekar <atharvakalekar06@gmail.com>
2025-01-02 23:09:15 +0530

    Add files via upload

---
+++ dob.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class dob
+{
+    private:
+    int x;
+    int y;
+
+
+    
+    public:
+
+    void getdata();
+    {
+        cout<<"Emter the  year ";
+        cin>>x;
+        cout<<"Enter your birth year"
+
+      
+    }
+
+    void putdata()
+    {
+        cout<<x<<y<<endl;
+
+    }
+
+    dob operator(dob obj)
+    {
+        dob ans;
+        ans=x1-x2;
+        reutrn ans;
+    }
+
+    
+};
+
+int main()
+{
+    dob (x1,x2);
+     
+}
