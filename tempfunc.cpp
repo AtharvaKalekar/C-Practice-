3dfb31b6f53f9350b4be48c6bb420461b33c1ead
AtharvaKalekar <atharvakalekar06@gmail.com>
2025-01-04 17:13:06 +0530

    Add files via upload

---
+++ tempfunc.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+
+template<class T>
+
+T add(T &a,T &b)
+{
+    T result = a+b;
+    return result;
+
+}
+int main()
+{
+    int i=2;
+    int j=3;
+    float m= 1.2;
+    float n=2.3;
+    cout<<"Addition of i and j is :"<<add(i,j);
+    cout<<"\n";
+    cout<<"Addition of m and n is :"<<add(m,n);
+    cout<<"\n";
+}
